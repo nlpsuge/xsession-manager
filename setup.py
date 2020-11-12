7fbe652ef47b7aaa04eecf17f0aa9ab165e3bfd6
m^n <2271720+nlpsuge@users.noreply.github.com>
2020-11-12 22:50:02 +0800

    Remove a comment

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     # pip install xsession-manager
     name='xsession-manager',
-    version='0.0.1',  # Required
+    version='0.0.1',
     description='A command line to save and restore sessions for X11 desktops like Gnome, with many other features',
     long_description=long_description,
     long_description_content_type='text/markdown',
